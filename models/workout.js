d787f703a64b5ab9aa383669904725ffe51361ab
Drew <bridgmand@gmail.com>
2021-03-13 10:07:30 -0500

    setup file basics

---
+++ models/workout.js
@@ -0,0 +1,10 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+const WorkoutSchema = new Schema({
+   
+  });
+
+const Workout = mongoose.model("Transaction", WorkoutSchema);
+
+module.exports = Transaction;

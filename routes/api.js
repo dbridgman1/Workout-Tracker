7cd30213e7c685233c2a2508f7872c81f3df60dc
Drew <bridgmand@gmail.com>
2021-03-14 13:16:05 -0400

    added api routes

--- routes/api.js
+++ routes/api.js
@@ -1,5 +1,53 @@
 const router = require("express").Router();
-const db = require("../models");
+const Workout  = require("../models/workout");
 
+router.post("/api/workouts", (req, res) => {
+    Workout.create(req.body)
+      .then((dbWorkout) => {
+        res.json(dbWorkout);
+      })
+      .catch((err) => {
+        res.status(400).json(err);
+      });
+  });
+  
+  router.put("/api/workouts/:id", (req, res) => {
+    console.log(req.body);
+    Workout.findOneAndUpdate(
+      { _id: req.params.id },
+      { $push: { exercises: req.body } },
+      { new: true }
+    ).then((dbWorkout) => res.json(dbWorkout));
+  });
+  
+  router.get("/api/workouts", (req, res) => {
+    Workout.find({})
+      .sort({ date: -1 })
+      .then((dbWorkout) => {
+        res.json(dbWorkout);
+      })
+      .catch((err) => {
+        res.status(400).json(err);
+      });
+  });
+  
+  router.get("/api/workouts/range", (req, res) => {
+    Workout.aggregate([
+      {
+        $addFields: {
+          totalDuration: { $sum: "$exercises.duration" },
+        },
+      },
+    ])
+      .sort({ date: -1 })
+      .limit(7)
+      .then((dbWorkout) => {
+        res.json(dbWorkout);
+      })
+      .catch((err) => {
+        res.status(400).json(err);
+      });
+  });
+  
 
 module.exports = router;
